a80d13da9a398593da4152d61c0e72e84aade30e
Chris Farah <farahca@mail.uc.edu>
2025-04-25 22:16:06 -0400

    first commit

---
+++ src/main/java/us/feyrouzbackend/backend/FeyrouzbackendApplication.java
@@ -0,0 +1,13 @@
+package us.feyrouzbackend.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeyrouzbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeyrouzbackendApplication.class, args);
+	}
+
+}
